863b65e32095d9732c06d124d961e6cecc73804d
Sudharsan T <sudharsant2005@gmail.com>
2024-09-27 09:09:49 +0530

    Time: 22 ms (5.68%), Space: 50.9 MB (11.3%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,21 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int [][] dp = new int[s.length()+1][t.length()+1];
+        int [][] dp = new int[s.length()][t.length()];
         for(int row=0;row<s.length();row++){
             for(int col=0;col<t.length();col++){
                 dp[row][col] = -1;
             }
         }
 
-            return solve(dp,s,t,0,0);
+            return solve(dp,s,t,s.length()-1,t.length()-1);
         }
 
         private int solve(int[][] dp,String s, String t, int row, int col){
-            if(col == t.length()){
+            if(col<0){
                 return 1;
 
             }
-            if(row == s.length()){
+            if(row<0){
                 return 0;
             }
             if(dp[row][col] != -1){
@@ -24,10 +24,10 @@ class Solution {
 
 
             if(s.charAt(row) == t.charAt(col)){
-                dp[row][col] = solve(dp,s,t,row+1,col+1)+solve(dp,s,t,row+1,col);
+                dp[row][col] = solve(dp,s,t,row-1,col-1)+solve(dp,s,t,row-1,col);
             }
             else{
-                dp[row][col] = solve(dp,s,t,row+1,col);
+                dp[row][col] = solve(dp,s,t,row-1,col);
             }
             return dp[row][col];
         }
