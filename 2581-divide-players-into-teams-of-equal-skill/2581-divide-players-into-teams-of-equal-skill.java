e3a694070d1b73af2632ae0795a6fc351defe066
Sudharsan T <sudharsant2005@gmail.com>
2024-10-04 09:29:26 +0530

    Time: 17 ms (36.78%), Space: 57.8 MB (25.94%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,38 +1,29 @@
 class Solution {
-
     public long dividePlayers(int[] skill) {
-        int n = skill.length;
-        int totalSkill = 0;
-        int[] skillFrequency = new int[1001];
-
-        // Calculate total skill and skill frequency
-        for (int playerSkill : skill) {
-            totalSkill += playerSkill;
-            skillFrequency[playerSkill]++;
-        }
-
-        // Check if total skill can be evenly distributed among teams
-        if (totalSkill % (n / 2) != 0) {
-            return -1;
-        }
-
-        int targetTeamSkill = totalSkill / (n / 2);
-        long totalChemistry = 0;
-
-        // Calculate total chemistry while verifying valid team formations
-        for (int playerSkill : skill) {
-            int partnerSkill = targetTeamSkill - playerSkill;
-
-            // Check if a valid partner exists
-            if (skillFrequency[partnerSkill] == 0) {
+        int N = skill.length;
+        int a;
+        long sum = 0;
+        Arrays.sort(skill);
+        int tot = skill[0]+skill[N-1];
+         if(N == 2){
+                return skill[0]*skill[1];
+            }
+        for(int i=0;i<N;i++){
+           
+            if(skill[i]+skill[N-i-1] == tot ){
+                a = skill[i]*skill[N-i-1];
+                sum += a;
+                
+            }
+            else{
                 return -1;
             }
+            
 
-            totalChemistry += (long) playerSkill * (long) partnerSkill;
-            skillFrequency[partnerSkill]--;
+
         }
-
-        // Return half of totalChemistry as each pair is counted twice
-        return totalChemistry / 2;
+        return sum/2;
+        
+        
     }
 }
