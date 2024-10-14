7952571fb19e96e7825287574d81b0b20ff1cb1f
Sudharsan T <sudharsant2005@gmail.com>
2024-10-14 10:34:52 +0530

    Time: 3 ms (81.12%), Space: 45.9 MB (18.26%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -9,20 +9,15 @@ class Solution {
                 candies[i+1] = candies[i]+1;
             }
         }
-
         for(int i=N-1;i>0;i--){
             if(ratings[i] < ratings[i-1]){
                 candies[i-1] = Math.max(candies[i-1], candies[i]+1);
             }
         }
-
         for(int i=0;i<N;i++){
             sum += candies[i];
         }
 
         return sum;
-
-        
-        
     }
 }
