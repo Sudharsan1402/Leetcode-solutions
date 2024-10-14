8c1ff78a72270b8a9b590cc304bfecc338da84a9
Sudharsan T <sudharsant2005@gmail.com>
2024-10-14 10:34:11 +0530

    Time: 3 ms (81.12%), Space: 45.3 MB (70.81%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int N = ratings.length;
+        int sum = 0;
+        int [] candies = new int [N];
+        Arrays.fill(candies, 1);
+        for(int i=0;i<N-1;i++){
+            if(ratings[i] < ratings[i+1]){
+                candies[i+1] = candies[i]+1;
+            }
+        }
+
+        for(int i=N-1;i>0;i--){
+            if(ratings[i] < ratings[i-1]){
+                candies[i-1] = Math.max(candies[i-1], candies[i]+1);
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            sum += candies[i];
+        }
+
+        return sum;
+
+        
+        
+    }
+}
