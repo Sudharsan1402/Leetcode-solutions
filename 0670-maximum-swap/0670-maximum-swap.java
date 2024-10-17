c3300c31cd6f804c29b3cda2421f9a0935886215
Sudharsan T <sudharsant2005@gmail.com>
2024-10-17 10:05:40 +0530

    Time: 0 ms (100%), Space: 40.7 MB (21.01%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,26 +1,27 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] digits = Integer.toString(num).toCharArray();
-        int n = digits.length;
-        int[] rightMaxIdx = new int[n];
-        rightMaxIdx[n - 1] = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (digits[i] > digits[rightMaxIdx[i + 1]]) {
-                rightMaxIdx[i] = i;
-            } else {
-                rightMaxIdx[i] = rightMaxIdx[i + 1];
+        char [] digits = Integer.toString(num).toCharArray();
+        int N = digits.length;
+        int [] arr = new int [N];
+        arr[N-1] = N-1;
+        for(int i=N-2;i>=0;i--){
+            if(digits[i] > digits[arr[i+1]]){
+                arr[i] = i;
+            }
+            else{
+                arr[i] = arr[i+1];
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (digits[i] != digits[rightMaxIdx[i]]) {
-                swap(digits, i, rightMaxIdx[i]);
+        for(int i=0;i<N;i++){
+            if(digits[i] != digits[arr[i]]){
+                swap(digits, i, arr[i]);
                 break;
             }
         }
         return Integer.parseInt(new String(digits));
     }
 
-    private void swap(char[] digits, int i, int j) {
+    private void swap(char [] digits, int i, int j){
         char temp = digits[i];
         digits[i] = digits[j];
         digits[j] = temp;
