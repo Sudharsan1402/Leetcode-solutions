bef63e1cb2640daeaa7e805535cf52432dc26078
Sudharsan T <sudharsant2005@gmail.com>
2024-10-21 16:14:20 +0530

    Time: 36 ms (43.14%), Space: 45.1 MB (31.05%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,25 +1,26 @@
 class Solution {
-    int Count;
-    public int maxUniqueSplit(String s) {
-        Count=0;
-        HashSet<String>set = new HashSet<>();
-        back_track(s,set,0);
-        return Count;
-    }
-    public void back_track(String s, HashSet<String> set, int index){
-        int n = s.length();
-        if(index==n){
-            Count = Math.max(Count,set.size());
-            return;
-        }
-        for(int i=index;i<n;i++){
-            String sub = s.substring(index,i+1);
-            if(!set.contains(sub)){
-                set.add(sub);
-                back_track(s,set,i+1);
-                set.remove(sub);
+
+    int ans = 0;
+    private void solve(int idx, String s, Set<String> set){
+        if(idx >= s.length()){
+            ans = Math.max(ans, set.size());
+            return ;
+        } 
+
+        for(int i = idx ; i < s.length(); i++){
+            String temp = s.substring(idx, i + 1);
+            if(!set.contains(temp)){
+                set.add(temp);
+                solve(i + 1, s, set);
+                set.remove(temp);
             }
         }
+
     }
-    
+
+    public int maxUniqueSplit(String s) {
+        solve(0, s, new HashSet<>());
+
+        return ans;
+    }
 }
