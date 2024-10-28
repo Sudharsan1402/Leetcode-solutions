723101d68ba060f796e60900895788a3b4476b67
Sudharsan T <sudharsant2005@gmail.com>
2024-10-28 13:38:04 +0530

    Time: 32 ms (90.63%), Space: 61.7 MB (24.31%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
@@ -1,31 +1,16 @@
 class Solution {
-
     public int longestSquareStreak(int[] nums) {
-        // Map to store the length of square streak for each number
-        Map<Integer, Integer> streakLengths = new HashMap<>();
-
         Arrays.sort(nums);
-
-        for (int number : nums) {
-            int root = (int) Math.sqrt(number);
-
-            // Check if the number is a perfect square and its square root is in the map
-            if (root * root == number && streakLengths.containsKey(root)) {
-                // If so, extend the streak from its square root
-                streakLengths.put(number, streakLengths.get(root) + 1);
-            } else {
-                // Otherwise, start a new streak
-                streakLengths.put(number, 1);
-            }
+        int [] arr = new int[100001];
+        int maxi = 0;
+        for(int i: nums)
+        {
+            int root = (int)Math.sqrt(i);
+            if(root * root != i) arr[i] = 1; //no predecessor
+            else arr[i] = arr[root] + 1; // extend streak from root
+            maxi = Math.max(maxi, arr[i]);
         }
-
-        // Find the maximum streak length
-        int longestStreak = 0;
-        for (int streakLength : streakLengths.values()) {
-            longestStreak = Math.max(longestStreak, streakLength);
-        }
-
-        // Return -1 if no valid streak found, otherwise return the longest streak
-        return longestStreak == 1 ? -1 : longestStreak;
+        if(maxi == 1) return -1;
+        else return maxi;
     }
 }
