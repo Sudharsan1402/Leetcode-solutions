fd9da55ad6e5fad4f9e166141956b2c603b4c95d
Sudharsan T <sudharsant2005@gmail.com>
2024-11-01 20:23:59 +0530

    Time: 31 ms (75.58%), Space: 45.5 MB (63.92%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,27 +1,22 @@
 class Solution {
+
     public String makeFancyString(String s) {
-        
-        StringBuilder sb=new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        int count=1;
-        sb.append(s.charAt(0));
-        st.push(s.charAt(0));
-        for (int i=1;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c!=st.peek()){
-                sb.append(c);
-                st.push(c);
-                count=1;
+        char prev = s.charAt(0);
+        int frequency = 1;
+        StringBuilder ans = new StringBuilder();
+        ans.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == prev) {
+
+                frequency++;
+            } else {
+
+                prev = s.charAt(i);
+                frequency = 1;
             }
-           else{
-            st.push(c);
-            count++;
-            if(count<3){
-                 sb.append(c);
-            }
-           }
+
+            if (frequency < 3) ans.append(s.charAt(i));
         }
-        String str = sb.toString();
-        return str;
+        return ans.toString();
     }
 }
