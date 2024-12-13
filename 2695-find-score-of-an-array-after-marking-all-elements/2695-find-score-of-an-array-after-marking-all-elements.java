dd78999ac84dcd4b77e58bdec5d16f673a1e630b
Sudharsan T <sudharsant2005@gmail.com>
2024-12-13 20:16:15 +0530

    Time: 105 ms (93.63%), Space: 61.1 MB (27.31%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,32 +1,37 @@
 class Solution {
 
     public long findScore(int[] nums) {
+        
         long ans = 0;
-        int[][] sorted = new int[nums.length][2];
-        boolean[] marked = new boolean[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            sorted[i][0] = nums[i];
-            sorted[i][1] = i;
+        int n = nums.length;
+        int [][] arr = new int[n][2];
+        boolean [] marked = new boolean[n];
+        for(int i=0;i<n;i++){
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
         }
 
-        Arrays.sort(sorted, (arr1, arr2) -> arr1[0] - arr2[0]);
+        Arrays.sort(arr, (arr1, arr2) -> arr1[0] - arr2[0]);
 
-        for (int i = 0; i < nums.length; i++) {
-            int number = sorted[i][0];
-            int index = sorted[i][1];
-            if (!marked[index]) {
-                ans += number;
+        for(int i=0;i<n;i++){
+            int index = arr[i][1];
+            if(!marked[index]){
+                ans += arr[i][0];
                 marked[index] = true;
-                if (index - 1 >= 0) {
-                    marked[index - 1] = true;
+                
+                if(index-1 >= 0){
+                    marked[index-1] = true;
                 }
-                if (index + 1 < nums.length) {
-                    marked[index + 1] = true;
+                if(index+1 < n){
+                    marked[index+1] = true;
                 }
             }
         }
 
         return ans;
+
+
+
+
     }
 }
